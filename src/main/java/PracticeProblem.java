2f817dfdd9b4372e10e8c8f2ee3a822b5af5b8aa
Karthikeya Arikirevula <karthikeya.arikirevula@student.tdsb.on.ca>
2025-04-03 03:03:59 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,25 +3,21 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static void bubbleSortString(String[] strings) {
+		boolean swap = true;
+		int k = strings.length;
 
-	public static void q1() {
-		//Write question 1 code here
+		for(int i = 0; i < k - 1 && swap; i ++) {
+			
+			for(int j = 0; j < k - i - 1; j++) {
+				if(strings[j].toLowerCase().compareTo(strings[j+1].toLowerCase()) > 0) {
+					//swap
+					String temp = strings[j];
+					strings[j] = strings[j+1];
+					strings[j+1] = temp;
+					swap = true;
+				}
+			}
+		}
 	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
